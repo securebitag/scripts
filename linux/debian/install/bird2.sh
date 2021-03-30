bdab99038fe7d776737a5cc82c5ed1f3e3984f89
Securebit AG <45911247+securebitag@users.noreply.github.com>
2021-03-30 10:37:54 +0200

    Update bird2.sh

--- linux/debian/install/bird2.sh
+++ linux/debian/install/bird2.sh
@@ -4,5 +4,6 @@ sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sed -i 's/#net.ipv6.conf.all.forwarding=1/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
 
 # Add repo key
+apt install -y gnupg2
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B188E2B695BD4743
 echo "deb http://bird.network.cz/debian/ buster main">/etc/apt/sources.list.d/bird2.list
