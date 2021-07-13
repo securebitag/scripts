4b91eb7a66d836cd7863f21f30146c01cb9590b0
Securebit AG <45911247+securebitag@users.noreply.github.com>
2021-07-13 21:26:21 +0200

    Update sda.sh

--- linux/debian/install/sda.sh
+++ linux/debian/install/sda.sh
@@ -1,16 +1,16 @@
 apt install -y lvm2 parted
 
 # Disk partitioning
-parted --script /dev/sda \
-mklabel gpt \
-mkpart primary 1MB 3MB \
-name 1 grub \
-set 1 bios_grub on \
-mkpart primary 3MB 503MB \
-name 2 boot \
-set 2 boot on \
-mkpart primary 503MB 100% \
-name 3 system
+parted --script /dev/sda\
+ mklabel gpt\
+ mkpart primary 1MB 3MB\
+ name 1 grub\
+ set 1 bios_grub on\
+ mkpart primary 3MB 503MB\
+ name 2 boot\
+ set 2 boot on\
+ mkpart primary 503MB 100%\
+ name 3 system
 
 # Remove old LVM settings
 vgremove lvm --force
